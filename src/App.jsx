db76f91e38592f1ceefdb7d7a6bbf749daeac49f
Samuel Batista Viana <samuel.viana.yl@gmail.com>
2025-06-04 16:12:40 -0300

    feat: Preparação de ambiente e adição das fontes do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./assets/pages/Home/Home";
+
 function App() {
   return (
     <>
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path="/" Component={ Home }/>
+          </Routes>
+        </div>
+      </Router>
     </>
   )
 }
---
+++ src/assets/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import './Home.css';
+
+export default function Home() {
+    return (
+        <>
+            <h1>MACONHA</h1>
+        </>
+    );
+}
