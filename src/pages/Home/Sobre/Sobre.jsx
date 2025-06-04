cc0062ec452c9b1b0d4492170f9fcf0f4ec41a26
lalibosso1 <146896466+lalibosso1@users.noreply.github.com>
2025-06-04 17:04:02 -0300

    feat: add Sobre page

---
+++ src/pages/Home/Sobre/Sobre.jsx
@@ -0,0 +1,17 @@
+import './Sobre.css';
+
+export default function Sobre() {
+    return (
+    <div>
+        <div className='container-h1'>
+            <h1>Sobre o projeto blabla</h1>
+        </div>
+
+        <div>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet magna augue, a scelerisque quam efficitur et. Proin quis porttitor ligula.</p>
+        </div>
+       
+    </div>
+
+    )
+}
