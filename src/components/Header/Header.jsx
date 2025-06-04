b3042e14d0ac7169c9d42238951770aa9e0ef463
Samuel Batista Viana <samuel.viana.yl@gmail.com>
2025-06-04 18:26:42 -0300

    feat: Atualização do header, incompleto

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,43 @@
 import owlterLogoBranco from "../../assets/images/owlter-branco.png";
 import owlterLogoPreto from "../../assets/images/owlter-preto.png";
 import "./Header.css";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const onScroll = () => {
+            setScrolled(window.scrollY > 50);
+        };
+
+        window.addEventListener('scroll', onScroll);
+        return () => window.removeEventListener('scroll', onScroll);
+    }, [])
+
     return (
         <>
             <header>
-                <nav className="nav-main">
-                    <img src={ owlterLogoBranco }/>
-                    <ul>
-                        <li>Home</li>
-                        <li>Sobre</li>
-                        <li>FAQ</li>
-                    </ul>
-                </nav>
+                 <div className={`container-nav-alt ${scrolled ? 'ativo' : ''}`}>
+                    <nav className="nav-alt" style={{ display: scrolled ? "flex" : "none" }}>
+                        <img src={ owlterLogoBranco }/>
+                        <ul>
+                            <li>Home</li>
+                            <li>Sobre</li>
+                            <li>FAQ</li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className={`container-nav-main ${scrolled ? 'ativo' : ''}`}>
+                    <nav className="nav-main" style={{ display: scrolled ? "none" : "flex" }}>
+                        <img src={ owlterLogoPreto }/>
+                        <ul>
+                            <li>Home</li>
+                            <li>Sobre</li>
+                            <li>FAQ</li>
+                        </ul>
+                    </nav>
+                </div>
             </header>
         </>
     );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,6 @@ import './Home.css';
 export default function Home() {
     return (
         <>
-            <Header />
         </>
     );
 }
