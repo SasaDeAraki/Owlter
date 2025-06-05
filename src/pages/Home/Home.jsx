5809a84eebd03ef88e17845182d0821e8e22d29d
Samuel Batista Viana <samuel.viana.yl@gmail.com>
2025-06-05 14:13:50 -0300

    feat: pequenas adições à home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
-import Header from '../../components/Header/Header';
 import './Home.css';
 import placeholder from '../../assets/images/placeholder.jpg'
+import icon from '../../assets/images/marker1.png'
 
 export default function Home() {
     return (
@@ -33,21 +33,49 @@ export default function Home() {
                 </div>
             </div>
             <div className='app-section'>
-                <div>
-                    <div>
+                <div className='app-text-section'>
+                    <div className='text-s-title'>
                         <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet magna augue, a scelerisque quam efficitur et. Proin quis porttitor ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </div>
-                    <div>
-                        <div>
-
+                    <div className='app-cards-section'>
+                        <div className='card-row'>
+                            <div className='card'>
+                                <img src={ icon }/>
+                                <div>
+                                    <h3>Lorem ipsum dolor sit.</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                </div>
+                            </div>
+                            <div className='card'>
+                                <img src={ icon }/>
+                                <div>
+                                    <h3>Lorem ipsum dolor sit.</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                </div>
+                            </div>
                         </div>
-                        <div>
-
+                        <div className='card-row'>
+                            <div className='card'>
+                                <img src={ icon }/>
+                                <div>
+                                    <h3>Lorem ipsum dolor sit.</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                </div>
+                            </div>
+                            <div className='card'>
+                                <img src={ icon }/>
+                                <div>
+                                    <h3>Lorem ipsum dolor sit.</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-
+                <div className='app-image-section'>
+                    <img src='#'/>
+                </div>
             </div>
         </>
     );
