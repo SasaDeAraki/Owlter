88ee43f98c40daeae1672476fa075e73e0ec9d9c
lalibosso1 <146896466+lalibosso1@users.noreply.github.com>
2025-06-05 20:27:09 -0300

    fix: Ajustes e correção dos placeholders

--- src/pages/Sobre/Sobre.jsx
+++ src/pages/Sobre/Sobre.jsx
@@ -3,17 +3,20 @@ import './Sobre.css';
 import img1 from './../../assets/images/img1.png'
 import img2 from './../../assets/images/img2.png'
 import img3 from './../../assets/images/img3.png'
-import icon1 from './../../assets/images/icon1.png'
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
+import icons1 from './../../assets/images/icons1.png';
+import icons2 from './../../assets/images/icons2.png';
+import icons3 from './../../assets/images/icons3.png';
+
 
 export default function Sobre() {
   const cards = [
-    { icon: icon1 , title: 'Lorem ipsum dolor.', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet magna augue, a scelerisque quam efficitur et. Proin quis porttitor ligula.' },
+    { icon: icons1 , title: 'Prevenção de tragédias', text: 'Muitos desastres naturais se agravam pela falta de informações rápidas e confiáveis. Nosso projeto busca reduzir riscos e salvar vidas ao alertar comunidades com antecedência.' },
     { image: img1 },
     { image: img2 },
-    { icon: icon1 , title: 'Lorem ipsum dolor.', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet magna augue, a scelerisque quam efficitur et. Proin quis porttitor ligula.' },
-    { icon: icon1 , title: 'Lorem ipsum dolor.', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet magna augue, a scelerisque quam efficitur et. Proin quis porttitor ligula.' },
+    { icon: icons2 , title: 'Fortalecimento da resposta local', text: 'Quando as pessoas compartilham relatos e atualizações, toda a comunidade se fortalece. Queremos incentivar a colaboração para que todos estejam mais preparados e protegidos.' },
+    { icon: icons3 , title: 'Minimizar impactos sociais e ambientais', text: 'Ao identificar rapidamente áreas de risco, ajudamos a evitar deslocamentos perigosos, reduzir perdas materiais e proteger o meio ambiente e a infraestrutura local.' },
     { image: img3 },
   ];
 
@@ -22,11 +25,11 @@ export default function Sobre() {
       <div className="sobre-projeto">
         <div className="sobre-topo">
           <div className='caixa-titulo'>
-            <h2>Sobre o projeto</h2>
-            <p className="subtitulo">Causa blabla</p>
+            <h2>Por que este</h2>
+            <p className="subtitulo"> projeto existe?</p>
           </div>
           <div className='caixa-descricao'>
-            <p className="descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet magna augue, a scelerisque quam efficitur et.</p>
+            <p className="descricao">Criamos uma plataforma colaborativa para monitorar desastres naturais em tempo real, promovendo segurança, prevenção e resposta rápida. Acreditamos que informação compartilhada pode salvar vidas.</p>
           </div>
         </div>
 
